0943411ea4a117227bc170e7e9aaa4ab874cbcca
magihub <77531918+magihub@users.noreply.github.com>
2026-04-11 22:20:18 +0800

    Update zhenjiang-epg.py

--- scripts/zhenjiang-epg.py
+++ scripts/zhenjiang-epg.py
@@ -15,6 +15,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from epg_common import merge_and_write, add_end_times
 from curl_cffi import requests
 
