db3637ed84be0a10479e6d29d9d84f721317ad38
magihub <77531918+magihub@users.noreply.github.com>
2026-04-12 13:47:06 +0800

    Update rudong-epg.py

--- scripts/rudong-epg.py
+++ scripts/rudong-epg.py
@@ -35,7 +35,7 @@ CHANNELS = [
         "api_url": "https://live.cm.jstv.com/api/Channel/ChannelInfoAudio",
         "params": {
             "channelId": 85,
-            "days": 2,          # 7为一周，0为页面节目单最后一天（也就是明天），1为包含明天和今天
+            "days": 7,          # 7为一周，0为页面节目单最后一天（也就是明天），1为包含明天和今天
             "globalId": "1244448"
         }
     }
